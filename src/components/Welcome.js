7304eda6c2ddc9bcbfe46753e54507410bc0e017
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-06-04 21:13:28 -0700

    created all components and style sheets. react-router dom working perfectly fine. copying over files one at a time

---
+++ src/components/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Welcome.css'
+
+function Welcome() {
+  return (
+    <div>
+      <body class="background">
+        <div class="index-container">
+          <article class="content">
+            <h1 class="title"> Yohanes Demissie </h1>
+            <h1 class="sub-title">Where critical thinking meets creative solutions</h1>
+            <a class="button" href="/home"><u>Click here</u></a>
+          </article>
+        </div>
+      </body>
+    </div>
+  )
+}
+
+export default Welcome;
